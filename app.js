24b3ef146fa8f2fad3b1245d20df35d89a9e74d2
Rahilsaleem12 <rahilsaleem565@gmail.com>
2023-04-20 02:25:04 +0500

    some code

--- app.js
+++ app.js
@@ -1,205 +1,227 @@
-// var totalmaks = 0;
-// var div = document.getElementById("section1");
+var totalmaks = 0;
+var div = document.getElementById("section1");
 
-// function submit1() {
-//   var input1 = document.getElementById("radio1");
-//   var input2 = document.getElementById("radio2");
-//   var input3 = document.getElementById("radio3");
+function submit1() {
+  var input1 = document.getElementById("radio1");
+  var input2 = document.getElementById("radio2");
+  var input3 = document.getElementById("radio3");
 
-//   if (input3.checked && input3.value === "Islamabad") {
-//     totalmaks += 2;
-//     console.log("Your answer is correct!");
-//     console.log(totalmaks);
-//   } else {
-//     console.log("Your answer is wrong!");
-//   }
-//   div.style.display = "none";
+  if (input3.checked && input3.value === "Islamabad") {
+    totalmaks += 2;
+    console.log("Your answer is correct!");
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+  }
+  div.style.display = "none";
 
-//   document.getElementById("section2").style.display = "block";
-//   calculateTotalMarks();
-// }
-// var time = 100;
-// var id = setInterval(function(){
-//   time--
-//   console.log(time);
-//   document.getElementById("timer1").innerText=time;
-// }
-// ,1000
-// )
-// setTimeout(() => {
-//   div.style.display = "none";
-//   document.getElementById("section2").style.display = "block";
-//   clearInterval(id)
-// }, 100000);
+  document.getElementById("section2").style.display = "block";
+  calculateTotalMarks();
+}
 
-// function submit2() {
-//   var input4 = document.getElementById("radio4");
-//   var input5 = document.getElementById("radio5");
-//   var input6 = document.getElementById("radio6");
 
-//   if (input6.checked && input6.value === "4") {
-//     console.log("Your answer is correct!");
-//     totalmaks += 2;
-//     console.log(totalmaks);
-//   } else {
-//     console.log("Your answer is wrong!");
-//     console.log(totalmaks);
-//   }
-//   var div = document.getElementById("section2");
-//   div.style.display = "none";
-//   document.getElementById("section3").style.display = "block";
-//   calculateTotalMarks();
-// }
+var time1 = 10;
+var id1 = setInterval(function(){
+  time1--
+//   console.log(time1);
+  document.getElementById("timer1").innerText=time1;
+ 
+}
+,1000
+)
+setTimeout(() => {
+  div.style.display = "none";
+  document.getElementById("section2").style.display = "block";
+  clearInterval(id1)
+}, 10000);
 
-// // function submi3() {
-// //   var input7 = document.getElementById("radio7");
-// //   var input8 = document.getElementById("radio8");
-// //   var input9 = document.getElementById("radio9");
 
-// //   if (input7.checked && input6.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section3");
-// //   div.style.display = "none";
-// //   document.getElementById("section4").style.display = "block";
+function submit2() {
+  var input4 = document.getElementById("radio4");
+  var input5 = document.getElementById("radio5");
+  var input6 = document.getElementById("radio6");
 
-// //   calculateTotalMarks();
-// // }
+  if (input6.checked && input6.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section2");
+  div.style.display = "none";
+  document.getElementById("section3").style.display = "block";
+  calculateTotalMarks();
+}
 
-// // function submit4() {
-// //   var input10 = document.getElementById("radio10");
-// //   var input11 = document.getElementById("radio11");
-// //   var input12 = document.getElementById("radio12");
+var time2 = 20;
+var id2 = setInterval(function(){
+  time2--;
+  document.getElementById("timer2").innerText = time2;
+}, 1000);
 
-// //   if (input11.checked && input6.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section4");
-// //   div.style.display = "none";
-// //   document.getElementById("section5").style.display = "block";
+setTimeout(() => {
+  div.style.display = "none";
+  document.getElementById("section2").style.display = "none";
+  document.getElementById("section3").style.display = "block";
+  clearInterval(id2);
+}, 20000);
 
-// //   calculateTotalMarks();
-// // }
+setTimeout(() => {
+  console.log(time2);
+}, 21000);
 
-// // function submit5() {
-// //   var input13 = document.getElementById("radio13");
-// //   var input14 = document.getElementById("radio14");
-// //   var input15 = document.getElementById("radio15");
 
-// //   if (input14.checked && input14.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section5");
-// //   div.style.display = "none";
-// //   document.getElementById("section6").style.display = "block";
-// //   calculateTotalMarks();
-// // }
+function submi3() {
+  var input7 = document.getElementById("radio7");
+  var input8 = document.getElementById("radio8");
+  var input9 = document.getElementById("radio9");
 
-// // function submit6() {
-// //   var input16 = document.getElementById("radio16");
-// //   var input17 = document.getElementById("radio17");
-// //   var input18 = document.getElementById("radio18");
+  if (input7.checked && input6.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section3");
+  div.style.display = "none";
+  document.getElementById("section4").style.display = "block";
 
-// //   if (input16.checked && input16.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section7");
-// //   div.style.display = "none";
-// //   calculateTotalMarks();
-// // }
+  calculateTotalMarks();
+}
 
-// // function submit7() {
-// //   var input19 = document.getElementById("radio19");
-// //   var input20 = document.getElementById("radio20");
-// //   var input21 = document.getElementById("radio21");
+function submit4() {
+  var input10 = document.getElementById("radio10");
+  var input11 = document.getElementById("radio11");
+  var input12 = document.getElementById("radio12");
 
-// //   if (input21.checked && input21.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section8");
-// //   div.style.display = "none";
-// //   calculateTotalMarks();
-// // }
+  if (input11.checked && input6.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section4");
+  div.style.display = "none";
+  document.getElementById("section5").style.display = "block";
 
-// // function submit8() {
-// //   var input22 = document.getElementById("radio22");
-// //   var input23 = document.getElementById("radio23");
-// //   var input24 = document.getElementById("radio24");
+  calculateTotalMarks();
+}
 
-// //   if (input22.checked && input22.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section9");
-// //   div.style.display = "none";
-// //   calculateTotalMarks();
-// // }
+function submit5() {
+  var input13 = document.getElementById("radio13");
+  var input14 = document.getElementById("radio14");
+  var input15 = document.getElementById("radio15");
 
-// // function submit9() {
-// //   var input25 = document.getElementById("radio25");
-// //   var input26 = document.getElementById("radio26");
-// //   var input27 = document.getElementById("radio27");
+  if (input14.checked && input14.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section5");
+  div.style.display = "none";
+  document.getElementById("section6").style.display = "block";
+  calculateTotalMarks();
+}
 
-// //   if (input25.checked && input25.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section10");
-// //   div.style.display = "none";
-// //   calculateTotalMarks();
-// // }
+function submit6() {
+  var input16 = document.getElementById("radio16");
+  var input17 = document.getElementById("radio17");
+  var input18 = document.getElementById("radio18");
 
-// // function submit10() {
-// //   var input28 = document.getElementById("radio28");
-// //   var input29 = document.getElementById("radio29");
-// //   var input30 = document.getElementById("radio30");
+  if (input16.checked && input16.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section7");
+  div.style.display = "none";
+  calculateTotalMarks();
+}
 
-// //   if (input30.checked && input30.value === "4") {
-// //     console.log("Your answer is correct!");
-// //     totalmaks += 2;
-// //     console.log(totalmaks);
-// //   } else {
-// //     console.log("Your answer is wrong!");
-// //     console.log(totalmaks);
-// //   }
-// //   var div = document.getElementById("section2");
-// //   div.style.display = "none";
-// //   calculateTotalMarks();
-// // }
+function submit7() {
+  var input19 = document.getElementById("radio19");
+  var input20 = document.getElementById("radio20");
+  var input21 = document.getElementById("radio21");
 
-// // function calculateTotalMarks() {
-// //   document.getElementById("marks").innerHTML = totalmaks;
-// // }
+  if (input21.checked && input21.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section8");
+  div.style.display = "none";
+  calculateTotalMarks();
+}
+
+function submit8() {
+  var input22 = document.getElementById("radio22");
+  var input23 = document.getElementById("radio23");
+  var input24 = document.getElementById("radio24");
+
+  if (input22.checked && input22.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section9");
+  div.style.display = "none";
+  calculateTotalMarks();
+}
+
+function submit9() {
+  var input25 = document.getElementById("radio25");
+  var input26 = document.getElementById("radio26");
+  var input27 = document.getElementById("radio27");
+
+  if (input25.checked && input25.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section10");
+  div.style.display = "none";
+  calculateTotalMarks();
+}
+
+function submit10() {
+  var input28 = document.getElementById("radio28");
+  var input29 = document.getElementById("radio29");
+  var input30 = document.getElementById("radio30");
+
+  if (input30.checked && input30.value === "4") {
+    console.log("Your answer is correct!");
+    totalmaks += 2;
+    console.log(totalmaks);
+  } else {
+    console.log("Your answer is wrong!");
+    console.log(totalmaks);
+  }
+  var div = document.getElementById("section2");
+  div.style.display = "none";
+  calculateTotalMarks();
+}
+
+function calculateTotalMarks() {
+  document.getElementById("marks").innerHTML = totalmaks;
+}
